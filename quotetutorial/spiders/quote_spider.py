8f08caf28ad027f6d7c5a6743228423bed1f8079
Kojo Asamoa-Mensah <kwadwoasamoa7@gmail.com>
2021-12-18 23:26:25 +0000

    initial commit

---
+++ quotetutorial/spiders/quote_spider.py
@@ -0,0 +1,26 @@
+import scrapy
+from ..items import QuotetutorialItem
+
+class QuoteSpider(scrapy.Spider):
+    name = 'quotes'
+    start_urls = [
+        "http://quotes.toscrape.com/"
+        
+    ]
+
+    def parse(self, response):
+
+        item = QuotetutorialItem()
+
+        all_div_quotes = response.css('div.quote')
+
+        for quote in all_div_quotes:
+            title = quote.css('span.text::text').extract()
+            author = quote.css('.author::text').extract()
+            tag = quote.css('.tag::text').extract()
+
+            item['title'] = title
+            item['author'] = author
+            item['tag'] = tag
+
+            yield item
